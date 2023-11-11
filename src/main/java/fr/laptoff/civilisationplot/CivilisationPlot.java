956b53803eeaf4285e6c9e040cdffb36908ae452
laptoff <laptoff.contact@gmail.com>
2023-11-11 19:44:17 +0100

    Init Paper project

---
+++ src/main/java/fr/laptoff/civilisationplot/CivilisationPlot.java
@@ -0,0 +1,19 @@
+package fr.laptoff.civilisationplot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CivilisationPlot extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
