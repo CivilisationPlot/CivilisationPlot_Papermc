3242c080b733b30ed6fcb191b1e5ed334d56a70f
laptoff <laptoff.contact@gmail.com>
2024-03-03 21:58:09 +0100

    add the FileManager class.

--- src/main/java/fr/laptoff/civilisationplot/managers/civil/Civil.java
+++ src/main/java/fr/laptoff/civilisationplot/managers/civil/Civil.java
@@ -1,5 +1,8 @@
 package fr.laptoff.civilisationplot.managers.civil;
 
+import fr.laptoff.civilisationplot.CivilisationPlot;
+
+import java.io.File;
 import java.util.List;
 import java.util.UUID;
 
@@ -8,11 +11,15 @@ public class Civil {
     private final UUID Uuid;
     private final float Money;
     private final List<UUID> Friends;
+    private final jsonManager json = new jsonManager();
 
     public Civil(UUID uuid, float money, List<UUID> friends){
         this.Uuid = uuid;
         this.Money = money;
         this.Friends = friends;
+
+        CivilisationPlot plugin = CivilisationPlot.getInstance();
+        File file = new File(plugin.getDataFolder() + "/Data/Civil/" + this.Uuid + ".yml");
     }
 
     public UUID getUuid(){
@@ -27,6 +34,12 @@ public class Civil {
         return this.Friends;
     }
 
+    public String getCivilJson(){
+        return json.serialize(this);
+    }
 
+    public void localSave(){
+
+    }
 
 }
